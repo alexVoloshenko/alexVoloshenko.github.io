891d2a8933eb414279e792003b336b42e4b3de31
Oleksand Voloshenko <alex.voloshenkoua@gmail.com>
2019-01-28 11:58:15 +0200

    refactoring and update

---
+++ todo-2/precache-manifest.ceb16d7ef6c199dcb6f692f2cea608fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5ca5a660e22387fc0c3",
+    "url": "/static/js/main.e5ca5a66.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "e5ca5a660e22387fc0c3",
+    "url": "/static/css/main.2c3ea0f5.chunk.css"
+  },
+  {
+    "revision": "867cbf988c0648518da69525eb72ecd1",
+    "url": "/index.html"
+  }
+];
