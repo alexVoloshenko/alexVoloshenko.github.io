49f4174578993474a64a1faa4df8bf2ba3d187ea
alexVoloshenko <40388120+alexVoloshenko@users.noreply.github.com>
2019-04-12 04:52:01 +0300

    Add files via upload

---
+++ elementary/precache-manifest.4d41f93a680fb66c01b59ca9323328ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ad457c6074d91488cb2",
+    "url": "/elementary/static/js/runtime~main.288270d3.js"
+  },
+  {
+    "revision": "b0740fa190f38ac77dc8",
+    "url": "/elementary/static/js/main.032822bc.chunk.js"
+  },
+  {
+    "revision": "472310964b8870ad6c33",
+    "url": "/elementary/static/js/2.78ba3352.chunk.js"
+  },
+  {
+    "revision": "b0740fa190f38ac77dc8",
+    "url": "/elementary/static/css/main.077abab9.chunk.css"
+  },
+  {
+    "revision": "b1af688ef263461aaec9d2754933a702",
+    "url": "/elementary/index.html"
+  }
+];
