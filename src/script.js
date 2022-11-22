ae9f0f01983685ff982871c0a016cf363489e0d2
Alex Voloshenko <alex.voloshenkoua@gmail.com>
2022-11-22 10:24:12 +0200

    test

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
-async function getFetch(url) {
-    let res = await fetch(url)
+function getFetch(url) {
+    let res = fetch(url)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            return data;
+            console.log(data);
         });
     return res;
 }
-console.log(getFetch('https://site.bookimed.com/widget/before-after-operation/clinic/1169?lang=en'))
+getFetch('https://site.bookimed.com/widget/before-after-operation/clinic/1169?lang=en')
