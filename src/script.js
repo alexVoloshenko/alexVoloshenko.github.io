5a81aefd9e592a4870dcfb514940b9cbf7faa2b9
Alex <alex.voloshenkoua@gmail.com>
2022-11-24 22:57:24 +0200

    test

--- src/script.js
+++ src/script.js
@@ -8,4 +8,20 @@ function getFetch(url) {
         });
     return res;
 }
-getFetch('https://site.bookimed.com/widget/before-after-operation/clinic/1169?lang=en')
+const containers = document.getElementsByClassName('offer-card__img-container--test')
+containers.forEach(element => {
+    element.addEventListener('scroll', function(){
+        console.log("scroll")
+    })
+});
+// containers.addEventListener('scroll', function(){
+//     console.log("here")
+// })
+const imgs = document.getElementsByClassName('offer-card__amp-img')
+imgs.forEach(element => {
+    element.addEventListener('click', function(){
+        console.log("click")
+    })
+});
+
+//getFetch('https://site.bookimed.com/widget/before-after-operation/clinic/1169?lang=en')
