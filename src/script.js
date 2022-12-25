7419886bdb531b4604c5005a2203d7f1676153e1
Alex Voloshenko <alex.voloshenkoua@gmail.com>
2022-12-26 01:12:27 +0200

    s

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ let script = document.querySelectorAll('.amp-script')
 console.log(script)
 const button  = document.querySelector('.button')
 button.addEventListener('click', (e)=> {
-    e.target.style.color = 'red'
-    console.log(e)
+    const text = document.querySelector('.text')
+    text.style.display = 'unset'
+    e.target.style.display = 'none'
 })
