281efa353fa3fd51d2c85b57131808005fa47b64
Alex Voloshenko <alex.voloshenkoua@gmail.com>
2022-12-26 01:00:20 +0200

    s

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 let script = document.querySelectorAll('.amp-script')
 console.log(script)
-const button  = document.querySelector('button')
+const button  = document.querySelector('.button')
 button.addEventListener('click', (e)=> {
     e.target.style.color = 'red'
     console.log(e)
