b23935e4d2c882aa6e202d7c81a52ec34e894e07
Alex Voloshenko <alex.voloshenkoua@gmail.com>
2022-12-26 00:37:24 +0200

    r

--- src/script.js
+++ src/script.js
@@ -1,2 +1,6 @@
 let script = document.querySelectorAll('.amp-script')
 console.log(script)
+const button  = document.querySelector('button')
+button.addEventListener('click', (e)=> {
+    console.log(e)
+})
